cd18ab077e4f888aced2b87e642bbf5cfb4a2c48
admircejvanovic <28600628+ado23@users.noreply.github.com>
2022-09-23 01:39:53 +0200

    Changed to HashRouter.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Provider } from "react-redux";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
